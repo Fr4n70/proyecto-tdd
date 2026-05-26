9118af4785bb94c5f1e4e16ae22fc5cd726e35a3
Fr4n70 <liliabyby@gmail.com>
2026-05-25 19:43:40 -0500

    feat: implementar listado de eventos (Green)

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,8 +7,17 @@ use App\Models\Event;
 use App\Http\Requests\StoreEventRequest;
 use Illuminate\Http\RedirectResponse;
 
+
+
 class EventController extends Controller
 {
+
+    public function index()
+    {
+        $events = Event::all();
+        return view('events.index', ['events' => $events]);
+    }
+
     public function store(StoreEventRequest $request): RedirectResponse
     {
         $eventData = $request->all();
